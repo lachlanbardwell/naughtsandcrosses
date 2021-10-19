f1656aaaf3592aa1593183cfd15921abdbe33a65
Lachlan Bardwell <75568181+KnowsJudo@users.noreply.github.com>
2021-10-20 01:41:41 +1100

    publicpath

--- webpack.config.js
+++ webpack.config.js
@@ -17,7 +17,7 @@ module.exports = {
   entry: path.resolve(__dirname, 'src', 'client', 'index.tsx'),
   output: {
     path: path.resolve(__dirname, 'dist'),
-    publicPath: isDevelopment ? '' : '/dist/',
+    publicPath: '',
     filename: 'bundle.js',
   },
   resolve: {
