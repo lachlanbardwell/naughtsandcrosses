7228f236eed74070b0f00ccd5f212d79352af9c5
Lachlan Bardwell <75568181+KnowsJudo@users.noreply.github.com>
2021-11-03 23:47:20 +1100

    footer + css

--- src/client/components/footer/footer.tsx
+++ src/client/components/footer/footer.tsx
@@ -1,10 +1,52 @@
 import React from 'react';
 import '../footer/footer.scss';
+import facebook from '../../../shared/images/facebook-white.svg';
+import instagram from '../../../shared/images/instagram-white.svg';
+import linkedin from '../../../shared/images/linkedin-white.svg';
+import twitter from '../../../shared/images/twitter-white.svg';
+import youtube from '../../../shared/images/youtube-white.svg';
+
+const socials: string[] = [facebook, instagram, linkedin, twitter, youtube];
+const otherLinks: string[] = [
+  'Terms & Conditions',
+  'Legal Disclaimer',
+  'About Lachie B',
+  'Testimonials',
+  'Contact Us',
+];
 
 export const LachFooter: React.FC = () => {
   return (
     <footer>
-      <p>&copy; Lachlan Bardwell 2021</p>
+      <nav className="social">
+        <ul>
+          {socials.map((next, ind) => (
+            <li key={ind}>
+              <a
+                href={
+                  next === linkedin
+                    ? 'https://www.linkedin.com/in/lachlan-bardwell'
+                    : '#'
+                }
+              >
+                <img src={next} alt={`Find us on ${next}`} />
+              </a>
+            </li>
+          ))}
+        </ul>
+      </nav>
+      <div className="copyFootRight">
+        <span className="legal">
+          {otherLinks.map((next, ind) => (
+            <p key={ind}>
+              <a href="#">{next}</a>
+            </p>
+          ))}
+        </span>
+      </div>
+      <div className="copyFootLeft">
+        <p>&copy; Lachlan Bardwell 2021</p>
+      </div>
     </footer>
   );
 };
